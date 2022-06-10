3d39fc792e6e5e30b16b6c502a5b24e11283d3d3
Cameron W <atealltheglue@protonmail.com>
2022-06-09 20:19:19 -0500

    sync

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <>
+    <div className="content">
       <Navbar />
       <div className="landing-container">
+        <div className="landing-text">
+          <span>&mdash; Welcome to &mdash;</span>
+          <span>Sigourney Area Development Corporation</span>
+        </div>
+        <div>
 
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 

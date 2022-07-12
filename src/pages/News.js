68fe2edb9b5aa0bc034389aad6b298814c04d45f
horsaen <atealltheglue@protonmail.com>
2022-07-11 19:29:30 -0500

    news page idea finished, will improve upon feedback

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import Seperator from '../components/Seperator';
@@ -32,9 +33,131 @@ function News() {
                                 </span>
                             </div>
                             <div className="news-card-button">
-                                <button>
-                                    <span>Learn More</span> 
-                                </button>
+                                <Link to="/">
+                                    <button>
+                                        <span>Learn More</span> 
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="news-card">
+                            <div className="news-card-date">
+                                <span>SADC | Jan 1, 1970</span> 
+                            </div>
+                            <div className="news-card-header">
+                                <span>SADC to replace CFO</span>
+                            </div>
+                            <div className="news-card-text">
+                                <span>
+                                    La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. 
+                                </span>
+                            </div>
+                            <div className="news-card-button">
+                                <Link to="/">
+                                    <button>
+                                        <span>Learn More</span> 
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="news-card">
+                            <div className="news-card-date">
+                                <span>SADC | Jan 1, 1970</span> 
+                            </div>
+                            <div className="news-card-header">
+                                <span>SADC to replace CFO</span>
+                            </div>
+                            <div className="news-card-text">
+                                <span>
+                                    La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. 
+                                </span>
+                            </div>
+                            <div className="news-card-button">
+                                <Link to="/">
+                                    <button>
+                                        <span>Learn More</span> 
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="news-card">
+                            <div className="news-card-date">
+                                <span>SADC | Jan 1, 1970</span> 
+                            </div>
+                            <div className="news-card-header">
+                                <span>SADC to replace CFO</span>
+                            </div>
+                            <div className="news-card-text">
+                                <span>
+                                    La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. 
+                                </span>
+                            </div>
+                            <div className="news-card-button">
+                                <Link to="/">
+                                    <button>
+                                        <span>Learn More</span> 
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="news-card">
+                            <div className="news-card-date">
+                                <span>SADC | Jan 1, 1970</span> 
+                            </div>
+                            <div className="news-card-header">
+                                <span>SADC to replace CFO</span>
+                            </div>
+                            <div className="news-card-text">
+                                <span>
+                                    La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. 
+                                </span>
+                            </div>
+                            <div className="news-card-button">
+                                <Link to="/">
+                                    <button>
+                                        <span>Learn More</span> 
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="news-card">
+                            <div className="news-card-date">
+                                <span>SADC | Jan 1, 1970</span> 
+                            </div>
+                            <div className="news-card-header">
+                                <span>SADC to replace CFO</span>
+                            </div>
+                            <div className="news-card-text">
+                                <span>
+                                    La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l'ère Heian, en l'an 1156 de notre ère. 
+                                    Elle doit son nom au fait qu'elle ait eu lieu durant l'ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l'époque, les Minamoto, 
+                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. 
+                                </span>
+                            </div>
+                            <div className="news-card-button">
+                                <Link to="/">
+                                    <button>
+                                        <span>Learn More</span> 
+                                    </button>
+                                </Link>
                             </div>
                         </div>
                     </div>

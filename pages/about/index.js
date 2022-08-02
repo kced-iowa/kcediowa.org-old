25fd9dc4b553fadaf212dddf1df33bf23d180408
horsaen <atealltheglue@protonmail.com>
2022-08-02 02:55:57 -0500

    improved member list

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 import axios from 'axios';
 import Navbar from '/components/Navbar';
 import Seperator from '/components/Seperator';
@@ -52,11 +53,21 @@ const About = () => {
                         <span>Members</span>
                         <Seperator />
                     </div>
-                    {members.map((member) =>
-                        <ul className={styles.list} key={member.id}>
-                            <li>{member.name} <span> | </span>{member.occupation}</li>
-                        </ul>
-                    )}
+                    <div className={styles.list}>
+                        {members.map((member) =>
+                            <div className={styles.member} key={member.id}>
+                                <div className={styles.memberSidebar}></div>
+                                <div className={styles.memberImage}>
+                                    <Image alt="" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.GpuviZ2dkD_w9tP198SLeQHaHa%26pid%3DApi&f=1" layout="fill" />
+                                </div>
+                                <div className={styles.memberInfo}>
+                                    <span>{member.name}</span>
+                                    <span>{member.occupation}</span>
+                                    <span>Since | 2015</span>
+                                </div>
+                            </div>
+                        )}
+                    </div>
                     <span className={styles.subtitle}>*non-voting members</span>
                 </div>
             </div>

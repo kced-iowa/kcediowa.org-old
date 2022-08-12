0b2bb13d4704f725690033acedb4326966ff7f15
horsaen <atealltheglue@protonmail.com>
2022-08-12 11:33:52 -0500

    head, weird api bug

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,14 @@
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import Head from 'next/head';
 import axios from 'axios';
 import Navbar from '/components/Navbar';
 import Seperator from '/components/Seperator';
 import Footer from '/components/Footer';
 import styles from './About.module.css';
+
+import { MdOutlineOpenInNew } from 'react-icons/md';
 
 const About = () => {
     const [members, setMembers] = useState([]);
@@ -15,7 +19,7 @@ const About = () => {
     }, []);
     const fetchAbout = () => {
         axios
-        .get('http://localhost:5000/api/about/')
+        .get('http://192.168.1.162:5000/api/about/')
         .then((res) => {
             console.log(res);
             setAbout(res.data);
@@ -26,7 +30,7 @@ const About = () => {
     };
     const fetchMembers = () => {
         axios
-        .get('http://localhost:5000/api/members/')
+        .get('http://192.168.1.162:5000/api/members/')
         .then((res) => {
             console.log(res);
             setMembers(res.data);
@@ -37,6 +41,9 @@ const About = () => {
     };
     return (
         <div>
+            <Head>
+                <title>About | SADC</title>
+            </Head>
             <Navbar />
             <div className={styles.about}>
                 <div className={styles.container}>
@@ -56,7 +63,9 @@ const About = () => {
                     <div className={styles.list}>
                         {members.map((member) =>
                             <div className={styles.member} key={member.id}>
-                                <div className={styles.memberSidebar}></div>
+                                <Link href={'/about/' + member.id}>
+                                    <div className={styles.memberSidebar}>{'<'}</div>
+                                </Link>
                                 <div className={styles.memberImage}>
                                     <Image alt="" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.GpuviZ2dkD_w9tP198SLeQHaHa%26pid%3DApi&f=1" layout="fill" />
                                 </div>

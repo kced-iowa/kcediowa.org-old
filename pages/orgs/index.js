effe9f9be86f99b50d11053146fe8962ae9ab6c4
horsaen <atealltheglue@protonmail.com>
2022-08-18 21:11:09 -0500

    starter

--- pages/orgs/index.js
+++ pages/orgs/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Navbar from '/components/Navbar';
 import Seperator from '/components/Seperator';
 import Footer from '/components/Footer';
+import styles from './Orgs.module.css';
 
 function Orgs() {
     return (
@@ -10,7 +11,9 @@ function Orgs() {
                 <title>Clubs & Organizations | SADC</title>
             </Head>
             <Navbar />
-            <div></div>
+            <div className={styles.page}>
+
+            </div>
             <Footer />
         </div>
     )
--- pages/visit/index.js
+++ pages/visit/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Navbar from '/components/Navbar';
 import Seperator from '/components/Seperator';
 import Footer from '/components/Footer';
+import styles from './Visit.module.css';
 
 function Visit() {
     return (
@@ -10,7 +11,9 @@ function Visit() {
                 <title>Visit | SADC</title>
             </Head>
             <Navbar />
-            <div></div>
+            <div className={styles.page}>
+
+            </div>
             <Footer />
         </div>
     )

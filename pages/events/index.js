c5e74582beb9c31fb3a9965002b8ec71ad412741
horsaen <atealltheglue@protonmail.com>
2022-08-23 00:24:10 -0500

    event styles, still awaiting tech int

--- pages/events/index.js
+++ pages/events/index.js
@@ -5,7 +5,7 @@ import Seperator from '/components/Seperator';
 import Footer from '/components/Footer';
 import styles from './Events.module.css';
 
-import { BsFillPinMapFill } from 'react-icons/bs'
+import { FaMapMarkerAlt } from 'react-icons/fa'
 
 function Events() {
     return (
@@ -25,18 +25,18 @@ function Events() {
                             <div className={styles.type}>
                                 <span>Farmer`s Market</span>
                             </div>
-                            <div className={styles.date}>
-                                <span className={styles.int}>24</span>
-                                <span>SEPT</span>
-                                <span>9:40 - 8:60</span>
-                            </div>
+                        </div>
+                        <div className={styles.date}>
+                            <span className={styles.int}>24</span>
+                            <span>SEPT</span>
+                            <span>9:40 - 8:60</span>
                         </div>
                         <div className={styles.details}>
                             <div className={styles.desc}>
                                 <span>Buy fresh vegetables at the farmers market</span>
                             </div>
                             <div className={styles.address}>
-                                <span><BsFillPinMapFill /> 15999 200th avenue</span> 
+                                <span><FaMapMarkerAlt /> 15999 200th avenue</span> 
                             </div>
                             <div className={styles.forms}>
                                 <Link href="https://forms.google.com/">

de1e2107301623b3960773ef1b94f47400ecb5cc
horsaen <atealltheglue@protonmail.com>
2023-03-28 23:44:22 -0500

    styles and swr migration

--- pages/events/index.js
+++ pages/events/index.js
@@ -4,6 +4,7 @@ import Navbar from '/components/Navbar';
 import Seperator from '/components/Seperator';
 import Footer from '/components/Footer';
 import { useEffect, useState } from 'react';
+import useSWR from 'swr'
 import axios from 'axios'
 import styles from './Events.module.css';
 
@@ -11,25 +12,46 @@ import { FaMapMarkerAlt } from 'react-icons/fa'
 
 const api = process.env.NEXT_PUBLIC_APIBASE
 
+const fetcher = url => axios.get(url).then(res => res.data)
+const key = api + '/events'
+
+function EventCard (props) {
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardHeader}>
+                <div className={styles.type}>
+                    <span>{props.title}</span>
+                </div>
+            </div>
+            <div className={styles.date}>
+                <span className={styles.int}>{props.dd}</span>
+                <span>{props.mm}</span>
+                <div>
+                    <span>{props.timestart}</span>
+                    <span> - </span>
+                    <span>{props.timeend}</span>
+                </div>
+            </div>
+            <div className={styles.details}>
+                <div className={styles.desc}>
+                    <span>{props.desc}</span>
+                </div>
+                <div className={styles.address}>
+                    <span><FaMapMarkerAlt /></span>
+                    <span>{props.address}</span> 
+                </div>
+                <div className={styles.forms}>
+                    <div className={styles.formButton}>
+                        <a rel="noreferrer" target="_blank" href={props.rsvp}>{props.rsvp}</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 function Events() {
-
-    const [data, setData] = useState([])
-    
-    useEffect(() => {
-        fetchEvents()
-    }, [])
-
-    const fetchEvents = () => {
-        axios
-        .get(api + '/events')
-        .then((res)=> {
-            setData(res.data)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    }
-
+    const { data, error } = useSWR(key, fetcher)
     return (
         <div>
             <Head>
@@ -42,38 +64,18 @@ function Events() {
                     <Seperator />
                 </div>
                 <div className={styles.content}>
-                    {data.map((event) => (
-                        <div className={styles.card}>
-                            <div className={styles.cardHeader}>
-                                <div className={styles.type}>
-                                    <span>{event.title}</span>
-                                </div>
-                            </div>
-                            <div className={styles.date}>
-                                <span className={styles.int}>{event.dd}</span>
-                                <span>{event.mm}</span>
-                                <div>
-                                  <span>{event.timestart}</span>
-                                  <span> - </span>
-                                  <span>{event.timeend}</span>
-                                </div>
-                            </div>
-                            <div className={styles.details}>
-                                <div className={styles.desc}>
-                                    <span>{event.desc}</span>
-                                </div>
-                                <div className={styles.address}>
-                                    <span><FaMapMarkerAlt /></span>
-                                    <span>{event.address}</span> 
-                                </div>
-                                <div className={styles.forms}>
-                                    <div className={styles.formButton}>
-                                        <a rel="noreferrer" target="_blank" href={event.rsvp}>{event.rsvp}</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+                {data && data.map((event)=> (
+                    <EventCard
+                        title={event.title}
+                        dd={event.dd}
+                        mm={event.mm}
+                        timestart={event.timestart}
+                        timeend={event.timeend}
+                        desc={event.desc}
+                        address={event.address}
+                        rsvp={event.rsvp}
+                    />
+                ))}
                 </div>
             </div>
             <Footer />
--- pages/news/index.js
+++ pages/news/index.js
@@ -3,9 +3,38 @@ import Link from 'next/link';
 import Navbar from '/components/Navbar';
 import Seperator from '/components/Seperator';
 import Footer from '/components/Footer';
+import useSWR from 'swr'
+import axios from 'axios'
 import styles from './News.module.css';
 
+const api = process.env.NEXT_PUBLIC_APIBASE
+
+const fetcher = url => axios.get(url).then(res => res.data)
+var key = api + '/news'
+
+function NewsCard (props) {
+    return (
+        <div className={styles.card}>
+            <div className={styles.cardDate}>
+                <span>{props.author + " | " + props.date}</span> 
+            </div>
+            <div className={styles.cardHeader}>
+                <span>{props.title}</span>
+            </div>
+            <div className={styles.cardText}>
+                <span>{props.metadata}</span>
+            </div>
+            <a rel='noreferrer' target='_blank' href={api + "/cdn/news/" + props.file}>
+                <div className={styles.cardButton}>
+                    <a><span>Learn More</span></a>
+                </div>
+            </a>
+        </div>
+        )
+}
+
 function News() {
+    const { data, error } = useSWR(key, fetcher)
     return (
         <div>
             <Head>
@@ -19,28 +48,15 @@ function News() {
                         <Seperator />
                     </div>
                     <div className={styles.content}>
-                        <div className={styles.card}>
-                            <div className={styles.cardDate}>
-                                <span>SADC | Jan 1, 1970</span> 
-                            </div>
-                            <div className={styles.cardHeader}>
-                                <span>Very Cool News Thing</span>
-                            </div>
-                            <div className={styles.cardText}>
-                                <span>
-                                    La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l&apos;ère Heian, en l&apos;an 1156 de notre ère. 
-                                    Elle doit son nom au fait qu&apos;elle ait eu lieu durant l&apos;ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l&apos;époque, les Minamoto, 
-                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto. La rébellion de Hōgen (保元の乱, Hōgen no ran?) est une courte guerre civile survenue dans le Japon de l&apos;ère Heian, en l&apos;an 1156 de notre ère. 
-                                    Elle doit son nom au fait qu&apos;elle ait eu lieu durant l&apos;ère Hōgen (1156-1159), et impliqua les trois plus puissants clans de l&apos;époque, les Minamoto, 
-                                    les Taira et les Fujiwara, dans une lutte pour la domination de la cour impériale de Kyoto.
-                                </span>
-                            </div>
-                            <Link href="/news/0">
-                                <div className={styles.cardButton}>
-                                    <a><span>Learn More</span></a>
-                                </div>
-                            </Link>
-                        </div>
+                        {data && data.map((news) => (
+                            <NewsCard
+                                author={news.author}
+                                date={news.date}
+                                title={news.title}
+                                metadata={news.metadata}
+                                file={news.file}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>

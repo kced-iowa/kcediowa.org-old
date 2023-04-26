26f191e7190fa2a2e07119311e29eb6c05480813
horsaen <atealltheglue@protonmail.com>
2023-04-26 00:04:16 -0500

    blablabla sync

--- pages/participate/index.js
+++ pages/participate/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { useState } from 'react'
 
 import Navbar from '/components/Navbar'
 import Seperator from '/components/Seperator'
@@ -7,6 +8,7 @@ import Footer from '/components/Footer'
 import styles from './Participate.module.css'
 
 export default function Participate () {
+    const [form, setForm] = useState('business')
     return (
         <>
             <Head>
@@ -19,13 +21,15 @@ export default function Participate () {
                     <Seperator />
                 </div>
                 <div className={styles.content}>
-                    <div>
-                        
-                    </div>
-                    <div>
-                        
-                    </div>
+                    <form id='business'>
+                        <input type="checkbox" checked onChange={()=>setForm('business')} />
+                    </form>
+                    <form id='individual'>
+                        <input type="checkbox" onChange={()=>setForm('individual')} />
+                    </form>
                 </div>
+                <buttom form={form} type='submit'>Submit</buttom>
+                <button type='reset'>Reset</button>
             </div>
             <Footer />
         </>
